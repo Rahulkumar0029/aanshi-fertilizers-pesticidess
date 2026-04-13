0e51440a50cf1ccdc4c43d26cc87e0a189cf9b0f
Rahul Kumar <165128816+Rahulkumar0029@users.noreply.github.com>
2026-04-13 13:19:15 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const BUSINESS_DETAILS = {
   businessName: "Aanshi Fertilizers & Pesticides",
   ownerName: "Anil Kumar Bishnoi",
-  phone: "9104779821",
+  phone: "+91 70146 39562",
   whatsapp: "919104779821",
   email: "aanshifarms@gmail.com",
   address: "30 Ps-A, Raisinghnagar, Sri Ganganagar, Rajasthan 335021",
